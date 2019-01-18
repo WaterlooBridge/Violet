5d26c5b8d6aa495cdcd00553d2f4a258730d002d
Sasuke <zhenlin1993@gmail.com>
2019-01-18 23:10:37 +0800

    add gallery

---
+++ src/main/java/com/zhenl/violet/media/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package com.zhenl.violet.media;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by winnie on 2017/7/26.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
